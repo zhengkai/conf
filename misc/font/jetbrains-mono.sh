bf96a87ee43f687216f3ad6ec7ce77348f8ad230
Zheng Kai <zhengkai@gmail.com>
2020-11-12 10:39:44 +0800

    up

--- misc/font/jetbrains-mono.sh
+++ misc/font/jetbrains-mono.sh
@@ -6,12 +6,8 @@ if [ ! -d JetBrainsMono ]; then
 	git clone --depth 1 git@github.com:JetBrains/JetBrainsMono.git
 fi
 
-cd JetBrainsMono/ttf
+cd JetBrainsMono/fonts/ttf
 
 git pull
-
-sudo cp ./*.ttf /usr/local/share/fonts
-
-cd 'No ligatures'
 
 sudo cp ./*.ttf /usr/local/share/fonts
