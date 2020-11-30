5fa5853de877f3518bf46fad104b0981e85df37b
Zheng Kai <zhengkai@gmail.com>
2020-11-30 17:46:17 +0800

    up

--- script/customize/daily.sh
+++ script/customize/daily.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+~/conf/script/safe-git-pull.sh ~/build
+~/conf/script/safe-git-pull.sh ~/hide
+~/conf/script/safe-git-pull.sh ~/conf
+
 ~/conf/update/daily.sh
--- script/safe-git-pull.sh
+++ script/safe-git-pull.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
+if [ -z "$1" ] || [ ! -d "$1" ]; then
 	>&2 echo input an path for git pull
 	exit 1
 fi
 
-cd $1 || exit 1
+cd "$1" || exit 1
 
-if [ -n "`git status -s 2>/dev/null`" ]; then
+if [ -n "$(git status -s 2>/dev/null)" ]; then
 	exit
 fi
 
-LOCAL=`git rev-parse HEAD`
-REMOTE=`git show-ref -s 'refs/remotes/origin/HEAD'`
-
-if [ "$LOCAL" == "$REMOTE" ]; then
-	git pull
-	git submodule update --init --recursive
-else
-	git fetch
+BRANCH=$(git branch --show-current)
+if [ -z "$BRANCH" ]; then
+	exit
 fi
 
-git gc
+git fetch
+
+if git merge-base --is-ancestor "origin/${BRANCH}" "$BRANCH"; then
+	git pull
+	git submodule update --init --recursive
+fi
+
+git gc --auto
