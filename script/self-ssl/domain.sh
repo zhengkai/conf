51e120672e3eaa47b7bdf872efa666476110a61d
Zheng Kai <zhengkai@gmail.com>
2021-03-29 16:54:00 +0800

    self-ssl

--- script/self-ssl/domain.sh
+++ script/self-ssl/domain.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # DOMAIN="edge-chat.messenger.com"
-DOMAIN="edge-chat.facebook.com"
+DOMAIN="${1:-edge-chat.facebook.com}"
+
+echo "domain: $DOMAIN"
 
 BASE="output/${DOMAIN}"
 
@@ -14,10 +16,13 @@ SUBJECT="/CN=${DOMAIN}/O=Soulogic/C=US"
 openssl genrsa -out "${BASE}.key" 2048
 openssl req -new -subj "$SUBJECT" -key "${BASE}.key" -out "${BASE}.csr" -passin "pass:$PASSPHRASE"
 
+cp ./tpl-v3.txt v3.ext
+sed -i "s#DOMAIN#${DOMAIN}#g" v3.ext
+
 openssl x509 -req -in "${BASE}.csr" \
 	-CA ca/root-ca.pem -CAkey ca/root-ca.key -CAcreateserial \
 	-extfile v3.ext -sha256 -days 1000 \
 	-out "${BASE}.crt"
 
-sudo cp output/edge-chat.messenger.com.crt /etc/nginx/ssl.d
-sudo cp output/edge-chat.messenger.com.key /etc/nginx/ssl.d
+echo sudo cp "${BASE}.crt" /etc/nginx/ssl.d
+echo sudo cp "${BASE}.key" /etc/nginx/ssl.d
