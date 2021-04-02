e32a7a345fdaf26e2e83643e8011cabc6a06e285
Zheng Kai <zhengkai@gmail.com>
2021-04-02 18:54:46 +0800

    up

--- script/ssl-0rtt.sh
+++ script/ssl-0rtt.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-host="ngx.hk"
-echo -e "GET / HTTP/1.1\r\nHost: $host\r\nConnection: close\r\n\r\n" > request.txt
-openssl s_client -connect $host:443 -tls1_3 -sess_out session.pem -ign_eof < request.txt
-echo | openssl s_client -connect $host:443 -tls1_3 -sess_in session.pem -early_data request.txt
+HOST="${1:-soulogic.com}"
+
+REQUEST="/tmp/request-0rtt.txt"
+SESSION="/tmp/session-0rtt.txt"
+
+echo -e "GET / HTTP/1.1\r\nHost: $HOST\r\nConnection: close\r\n\r\n" > "$REQUEST"
+openssl s_client -connect "${HOST}:443" -tls1_3 -sess_out "$SESSION" -ign_eof < "$REQUEST"
+echo | openssl s_client -connect "${HOST}:443" -tls1_3 -sess_in "$SESSION" -early_data "$REQUEST"
