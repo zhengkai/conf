e26251dd2aa81f55d16726387146d5dd0bacc6e2
Zheng Kai <zhengkai@gmail.com>
2021-04-13 11:37:54 +0800

    up

---
+++ misc/greasemonkey/tanksgg.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         tanks.gg 去广告
+// @namespace    https://soulogic.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Zheng Kai
+// @match        https://tanks.gg/*
+// @grant        none
+// ==/UserScript==
+//
+(() => {
+	setInterval(() => {
+		document.querySelectorAll('body > div').forEach(v => {
+			if (v?.style?.left === '10px') {
+				v.style.left = '-10000px';
+			}
+		});
+		const x = document.getElementById('ac-lre-player-ph')
+		if (x) {
+			x.style.display = 'none';
+		}
+	}, 500);
+})();
