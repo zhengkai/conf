186219cbb5aaedba05b9b1a5ecc100ec03e80c49
Zheng Kai <zhengkai@gmail.com>
2021-09-23 14:39:11 +0800

    reboot-check.sh

--- update/reboot-check.sh
+++ update/reboot-check.sh
@@ -1,19 +1,32 @@
 #!/bin/sh
 
-CHECK="/var/run/reboot-required"
-if [ ! -e "$CHECK" ]; then
-	exit
-fi
-cat "$CHECK"
-
-sudo apt clean
+DAY=6 # 超过天数后重启
 
 cd "$(dirname "$(readlink -f "$0")")" || exit 1
-
 FILE='last-reboot.log'
 
-TZ='Asia/Shanghai' date '+%Y-%m-%d %H:%M:%S' > "$FILE"
-echo >> "$FILE"
-cat /var/run/reboot-required.pkgs >> "$FILE"
+datetime() {
+	TZ='Asia/Shanghai' date '+%Y-%m-%d %H:%M:%S' > "$FILE"
+	echo >> "$FILE"
+}
 
-sudo reboot
+CHECK="/var/run/reboot-required"
+if [ -e "$CHECK" ]; then
+	cat "$CHECK"
+
+	sudo apt clean
+
+	datetime
+	cat "${CHECK}.pkgs" >> "$FILE"
+
+	sudo reboot
+fi
+
+UPSEC=$(awk '{print $1}' /proc/uptime)
+UPDAY=$(echo "${UPSEC}/86400" | bc)
+
+if [ "$UPDAY" -ge "$DAY" ]; then
+	datetime
+	echo "uptime over $UPDAY days (${UPTIME} sec)" >> "$FILE"
+	sudo reboot
+fi
