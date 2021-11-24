992364ce0e67f3fb41955e1db3b5289593b3ca06
Zheng Kai <zhengkai@gmail.com>
2021-11-24 18:46:44 +0800

    up

--- script/backup/freya-lydia.sh
+++ script/backup/freya-lydia.sh
@@ -1,24 +1,29 @@
 #! /bin/bash
 
-exec 200>"/tmp/rsync-freya-tesla.lock"
+HOST="${1:-freya}"
+
+echo "$HOST"
+exit
+
+exec 200>"/tmp/rsync-${HOST}-tesla.lock"
 flock -x -n 200 || {
 	>&2 echo
 	>&2 echo "$0 is running"
 	exit 1
 }
 
-source_path="freya:/log/"
-target_path="/backup/freya/log"
+source_path="${HOST}:/log/"
+target_path="/backup/${HOST}/log"
 
 rsync --temp-dir=/tmp --min-size=10 --partial -vzrtopg -e ssh "$source_path" "$target_path"
 
-ssh freya "find /log -type f -ctime +10        -delete"
-ssh freya "find /log -type d -ctime +1 -empty -delete"
+ssh "$HOST" "find /log -type f -ctime +10        -delete"
+ssh "$HOST" "find /log -type d -ctime +1  -empty -delete"
 
-source_path="freya:/backup/freya/"
-target_path="/backup/freya/db"
+source_path="${HOST}:/backup/${HOST}/"
+target_path="/backup/${HOST}/db"
 
 rsync --temp-dir=/tmp --min-size=10 --partial -vzrtopg -e ssh "$source_path" "$target_path"
 
-ssh freya "find /backup -type f -ctime +10        -delete"
-ssh freya "find /backup -type d -ctime +1  -empty -delete"
+ssh "$HOST" "find /backup -type f -ctime +10        -delete"
+ssh "$HOST" "find /backup -type d -ctime +1  -empty -delete"
