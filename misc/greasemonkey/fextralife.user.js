e622a932d15a8b13527e6cf865e45a1a1b570168
Zheng Kai <zhengkai@gmail.com>
2022-02-28 18:01:23 +0800

    up

--- misc/greasemonkey/fextralife.user.js
+++ misc/greasemonkey/fextralife.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         fextralife.com 去直播
 // @namespace    https://soulogic.com/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       Zheng Kai
 // @match        https://*.fextralife.com/*
@@ -15,7 +15,10 @@
 		x.innerHTML = '';
 	}
 
-	document.querySelectorAll('body > picture').forEach((pic) => {
-		pic.innerHTML = '';
-	});
+	const clean = () => {
+		document.querySelectorAll('body > picture, .ad-banner, .ad-siderbar, .hidden-sm, [data-adunit-name], .addthis-smartlayers').forEach(o => o.remove())
+	};
+
+	clean();
+	setInterval(clean, 500);
 })();
