c378d62364fc1344296820ed9f7bfce43ae3e49f
Zheng Kai <zhengkai@gmail.com>
2022-03-15 17:09:46 +0800

    up

--- misc/greasemonkey/zhihu.user.js
+++ misc/greasemonkey/zhihu.user.js
@@ -1,35 +1,22 @@
 // ==UserScript==
 // @name         知乎去提醒
 // @namespace    https://soulogic.com/
-// @version      0.6
+// @version      0.7
 // @description  try to take over the world!
 // @author       Zheng Kai
 // @match        https://www.zhihu.com/*
 // @match        https://zhihu.com/*
-// @grant        GM_addStyle
+// @grant        none
 // ==/UserScript==
 
 (() => {
     'use strict';
-
-    GM_addStyle('div.css-ybodb { display: none; }');
-    GM_addStyle('div.Popover18-toggle > div { display: none; }');
 
     const clean = () => {
 
 		if (document.hidden) {
 			return
 		}
-
-		/*
-        document.body.querySelectorAll('.Popover18-toggle > div').forEach(v => {
-            v.style.display = 'none';
-        });
-
-        document.body.querySelectorAll('div.RelatedCommodities-title, a.Banner-link').forEach(v => {
-            v.parentNode.parentNode.removeChild(v.parentNode);
-        });
-		 */
 
         document.title = document.title.replace(/\(.+?\) /, '');
 
@@ -41,12 +28,3 @@
     clean();
     setInterval(clean, 500);
 })();
-
-function GM_addStyle(cssStr) {
-    const D = document;
-    const newNode = D.createElement('style');
-    newNode.textContent = cssStr;
-
-    const targ = D.getElementsByTagName('head')[0] || D.body || D.documentElement;
-    targ.appendChild (newNode);
-}
