59e29b9536a4b0a5e8823f3213c3b2d9df1656c7
Zheng Kai <zhengkai@gmail.com>
2022-04-01 10:56:57 +0800

    up

--- update/clean-tmp.sh
+++ update/clean-tmp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -w /go/tmp ]; then
-	find /go/tmp -mtime +10 -delete
+	find /go/tmp -type f -mtime +10 -delete
 fi
 if [ -w ~/.tmp/vim-undo ]; then
-	find ~/.tmp/vim-undo -mtime +10 -delete
+	find ~/.tmp/vim-undo -type f -mtime +10 -delete
 fi
 if [ -w ~/.tmp/vim-swap ]; then
-	find ~/.tmp/vim-swap -mtime +10 -delete
+	find ~/.tmp/vim-swap -type f -mtime +10 -delete
 fi
 
 echo "h $HOST"
