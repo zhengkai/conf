955718305975632e3a796110d2afb98162aa7579
Zheng Kai <zhengkai@gmail.com>
2022-07-25 10:59:11 +0800

    up

--- misc/greasemonkey/twitter.user.js
+++ misc/greasemonkey/twitter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter Remove Promoted
 // @namespace    https://soulogic.com/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Zheng Kai
 // @match        https://twitter.com/*
@@ -13,8 +13,19 @@
 	const clean = () => {
 		document.querySelectorAll('span').forEach(a => {
 			if (a.innerText.startsWith('Promoted')) {
-				a?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode.remove();
+				const n = a?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode;
+				if (n) {
+					n.style.display = 'none';
+				}
 			}
+		});
+
+		document.querySelectorAll('video').forEach(a => {
+			const p = a?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode;
+			if (p) {
+				p.style.display = 'none';
+			}
+			a?.remove();
 		});
 	};
 
