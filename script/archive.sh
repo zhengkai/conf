789c909045daa6c28e79492c24d85732af94f91c
Zheng Kai <zhengkai@gmail.com>
2022-09-15 10:05:09 +0800

    up

--- script/archive.sh
+++ script/archive.sh
@@ -4,11 +4,13 @@ DATE=$(TZ="Asia/Shanghai" date "+%Y%m")
 echo "$DATE"
 
 function archive() {
-	SRC="$1"
 	DST="$2/${DATE}"
-	mkdir -p "$DST"
-	mv "${SRC}/"* "${DST}/"
+	# mkdir -p "$DST"
+	# mv "${SRC}/"* "${DST}/"
+	cd "$1" || exit 1
+	find . -type d -exec mkdir -p {} "${DST}"/{} \;
+	find . -type f -exec mv {} "${DST}"/{} \;
 }
 
 archive "/home/zhengkai/Pictures" "/mnt/sdb/pic"
-archive "/share" "/mnt/sdb/share"
+# archive "/share" "/mnt/sdb/share"
