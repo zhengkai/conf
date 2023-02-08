93b015ef3dd92688819f84d34e6da1f90e0369a8
Zheng Kai <zhengkai@gmail.com>
2023-02-08 15:16:42 +0800

    up

---
+++ misc/greasemonkey/dict.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         dict.cn
+// @namespace    https://soulogic.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Zheng Kai
+// @match        http://dict.cn/*
+// @match        https://dict.cn/*
+// @grant        none
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    const clean = () => {
+
+		if (document.hidden) {
+			return
+		}
+
+		document.querySelectorAll('iframe').forEach(v => v?.remove());
+		document.getElementById('aswift_0_host')?.remove();
+		document.getElementById('aswift_1_host')?.remove();
+
+		const o = document.getElementById('dict-chart-basic')?.style;
+		if (o) {
+			o.position = 'absolute';
+			o.right = 0;
+			o.top = 0;
+		}
+	};
+
+	clean();
+	setInterval(clean, 500);
+})();
