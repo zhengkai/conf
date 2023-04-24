f18364810b40d34804d020117e17e253a3ee575c
Zheng Kai <zhengkai@gmail.com>
2023-04-24 16:43:52 +0800

    bin/clean-autojump.sh

---
+++ bin/clean-autojump.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR=${1:-/www/}
+
+# 清理 autojump 数据里的特定目录，要从 / 起始
+#
+# 填 all 是全删
+
+cd ~/.local/share/autojump || exit 1
+# cd /tmp/autojump || exit 1
+
+if [ "$DIR" == "all" ]; then
+	rm autojump.txt || :
+	rm autojump.txt.bak || :
+	exit
+fi
+
+echo "clean dir $DIR in autojump"
+
+PATTERN=$(echo "$DIR" | sed 's/\//\\\//g')
+
+sed -i "/\t${PATTERN}/d" autojump.txt
