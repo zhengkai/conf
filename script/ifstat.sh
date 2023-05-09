ad3f6905513a1c0cf8cf96e205951a563447ecef
Zheng Kai <zhengkai@gmail.com>
2023-05-09 15:38:22 +0800

    up

--- script/ifstat.sh
+++ script/ifstat.sh
@@ -3,8 +3,8 @@
 IF=$(ifconfig -s | \grep -E '^(eth|enp|ens)' | awk '{print $1}' | tr '\n' ',' | sed 's/,$//')
 
 if [ -n "$IF" ]; then
-	ifstat -i "$IF"
+	ifstat -i "$IF" "$@"
 	exit
 fi
 
-ifstat
+ifstat "$@"
