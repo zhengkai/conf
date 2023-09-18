7883693821756933323eaaab17d9cfda88ee594f
Zheng Kai <zhengkai@gmail.com>
2023-09-18 17:19:02 +0800

    clean

--- script/cow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-(
-	flock -x -n 200 || exit 1
-
-	/home/zhengkai/.cow/cow
-
-) 200>/tmp/cow.lock
--- script/ss-config-to-url.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash -ex
-
-"$(dirname `readlink -f $0`)/ss-config-to-url.php" "$1" | qr
