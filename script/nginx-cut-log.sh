45c8635108dd0b6938adb121f841e82bc6817e56
Zheng Kai <zhengkai@gmail.com>
2023-09-19 15:20:18 +0800

    up

--- script/nginx-cut-log.sh
+++ script/nginx-cut-log.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-DIR=$(dirname `readlink -f $0`)
+DIR="$(dirname "$(readlink -f "$0")")" && cd "$DIR" || exit 1
 # "$DIR/fpm-cut-log.sh" 2>&1 || : &
 
 NGINX='/var/run/nginx.pid'
@@ -11,28 +11,28 @@ fi
 
 cd /log || exit 1
 
-DATE=`date --date='TZ="Asia/Shanghai" now' +'%Y%m/%d'`
+DATE=$(date --date='TZ="Asia/Shanghai" now' +'%Y%m/%d')
 
 readarray FILE < <( find . -type f \( -name 'access.log' -o -name 'error.log' \) -not -empty )
 
 echo ${#FILE[@]}
 
-if [ -z "$FILE" ]; then
+if [ -z "${#FILE[@]}" ]; then
 	>&2 echo 'no log found, exit'
 	exit
 fi
 
 CHANGE=''
 for L in "${FILE[@]}"; do
-	L=`echo "$L" | tr -d '\n'`
+	L=$(echo "$L" | tr -d '\n')
 
 	if [ ! -f "$L" ]; then
 		>&2 echo "unknown problem file $L, skip"
 		continue
 	fi
 
-	D=`dirname "$L"`
-	F=`basename "$L"`
+	D=$(dirname "$L")
+	F=$(basename "$L")
 
 	NEW="$D/$DATE-$F"
 	if [ -e "$NEW" ]; then
@@ -40,7 +40,7 @@ for L in "${FILE[@]}"; do
 		continue
 	fi
 
-	NEWD=`dirname "$NEW"`
+	NEWD=$(dirname "$NEW")
 
 	if [ ! -e "$NEWD" ]; then
 		mkdir -p "$NEWD"
@@ -55,5 +55,5 @@ for L in "${FILE[@]}"; do
 done
 
 if [ -n "$CHANGE" ]; then
-	sudo kill -USR1 `cat "$NGINX"`
+	sudo kill -USR1 "$(cat "$NGINX")"
 fi
