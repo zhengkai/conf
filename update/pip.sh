ef602bcefe2b212830cf40717477470a276fb4c6
Zheng Kai <zhengkai@gmail.com>
2024-07-09 11:09:56 +0800

    python venv

--- update/pip.sh
+++ update/pip.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [ ! -e ~/.venvs/default/bin/python ]; then
+	sudo apt install python3-venv
+	mkdir -p ~/.venvs
+	python3 -m venv ~/.venvs/default
+	hash -r
+fi
+
 pip freeze --local \
 	| grep -v '^\-e' \
 	| cut -d = -f 1  \
-	| xargs -n1 pip3 install --upgrade --no-warn-script-location
+	| xargs -n1 pip install --upgrade
 
-pip install --upgrade --no-warn-script-location httpie
-pip install --upgrade --no-warn-script-location qrcode
-pip install --upgrade --no-warn-script-location thefuck
-pip install --upgrade --no-warn-script-location pynvim
-pip install --upgrade --no-warn-script-location yt-dlp
+pip install --upgrade httpie
+pip install --upgrade qrcode
+pip install --upgrade thefuck
+pip install --upgrade pynvim
+pip install --upgrade yt-dlp
--- zsh/export.zsh
+++ zsh/export.zsh
@@ -14,7 +14,7 @@ export HERMES_DEFAULT_SIZE="18x18"
 
 export LC_ALL=en_US.UTF-8
 
-export PATH="$HOME/.local/bin:$HOME/conf/bin:$HOME/.npm/bin:$GOPATH/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/games:/snap/bin"
+export PATH="$HOME/.venvs/default/bin:$HOME/.local/bin:$HOME/conf/bin:$HOME/.npm/bin:$GOPATH/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/games:/snap/bin"
 
 export JQ_COLORS="48;5;237:48;5;52:48;5;22:38;5;80:38;5;214:1;39:1;39"
 
