d1bcc57b8943887860b02dc825643c6dc40ff1ba
Zheng Kai <zhengkai@gmail.com>
2024-10-21 17:28:13 +0800

    up

--- zsh/index.zsh
+++ zsh/index.zsh
@@ -57,18 +57,3 @@ fi
 [ -x "/usr/local/bin/ng" ] && source <(ng completion script)
 
 tabs 4
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/zhengkai/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/zhengkai/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/zhengkai/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/zhengkai/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
