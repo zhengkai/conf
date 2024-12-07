0c11e87988db8e1b9bb3bfa0f1ede8bdbc9e95c8
Zheng Kai <zhengkai@gmail.com>
2024-12-07 22:28:34 +0800

    fonts

---
+++ script/debug-font-use.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 检查每个字按系统顺序都实际用的哪个字体
+#
+# 用来验证 fonts.conf 的实际效果
+
+CHARS=("A" "中" "體" "の" "안" "😊" "م" "स्" "и")
+FAMILY=("sans-serif" "serif" "monospace")
+
+tabs 4
+for F in "${FAMILY[@]}"; do
+	echo
+	for CHAR in "${CHARS[@]}"; do
+		CODE=$(printf "%04X" "'${CHAR}'")
+		echo -en " $CHAR\t"
+		fc-match --format="%{file} %{family}\n" "${F}:charset=${CODE}"
+	done
+done
