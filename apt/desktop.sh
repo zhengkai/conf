321c2df4789947bc3ae87e94ff243047f798b809
Zheng Kai <zhengkai@gmail.com>
2025-05-22 16:00:10 +0800

    up

--- apt/desktop.sh
+++ apt/desktop.sh
@@ -13,4 +13,4 @@ sudo apt-get update
 sudo apt-get install -y "${list[@]}"
 
 mkdir -p ~/.config/kitty
-ln -s ~/conf/dotfiles/other/kitty.conf ~/.config/kitty
+ln -sf ~/conf/dotfiles/other/kitty.conf ~/.config/kitty
