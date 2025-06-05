4959921696766c9aac98cdfc06b6779da6b16ff0
Zheng Kai <zhengkai@gmail.com>
2025-06-05 09:46:38 +0800

    up

--- update/pip.sh
+++ update/pip.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+export PATH="${HOME}/.venvs/default/bin:${HOME}/.local/bin:${HOME}/.npm/bin:/go/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/snap/bin"
+
 if [ ! -e ~/.venvs/default/bin/python ]; then
 	sudo apt install python3-venv
 	mkdir -p ~/.venvs
 	python3 -m venv ~/.venvs/default
 	hash -r
+fi
+if [ ! -e ~/.venvs/default/bin/pip ]; then
+	exit 1
 fi
 
 pip install --upgrade pip
