e9d0e0b526461d152e515b26fb199782f3d247f7
Zheng Kai <zhengkai@gmail.com>
2025-12-11 13:10:25 +0800

    gm

--- misc/greasemonkey/twitter.user.js
+++ misc/greasemonkey/twitter.user.js
@@ -1,114 +1,19 @@
 // ==UserScript==
 // @name         Twitter Cleaner
 // @namespace    https://soulogic.com/
-// @version      2.3
-// @description  屏蔽所有广告、视频、名字上带国旗、互fo、蓝标的推，你要懒得自己部署可以直接用 https://k.9farm.com/gm/twitter.user.js
+// @version      2.4
+// @description  hide twitter link
 // @author       Zheng Kai
 // @match        https://x.com/*
 // @match        https://twitter.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
-//
-(() => {
 
-	let tweetPage = false;
-
-	const hideItem = [
-		'aside[aria-label="Subscribe to Premium"]',
-		'aside[aria-label="Get Verified"]',
-		'a[aria-label="Twitter Blue"]',
-		'a[aria-label="Communities"]',
-		'a[aria-label="Verified Orgs"]',
-		'a[aria-label="Premium"]',
-		'a[aria-label="Lists"]',
-		'a[aria-label="Lists"]',
-		'a[aria-label="Grok"]',
-		'a[aria-label="Verified"]',
-	].join(', ')
-
-	const forceHide = (a) => {
-		if (!a) {
-			return false;
-		}
-		a.style.display = 'none';
-		return true;
-		// a?.remove();
-	};
-
-	const hide = (a) => {
-		if (tweetPage || !a) {
-			return false;
-		}
-		a.style.display = 'none';
-		return true;
-		// a?.remove();
-	};
-
-	const hideArticle = (a) => {
-		if (!a?.closest('div[role=link]')) {
-			hide(a?.closest('article'));
-		}
-	};
-
-	const whiteList = [
-		'iamshaynez',
-		'PenngXiao',
-		'salonbus1024',
-		'PlayStation',
-		'armoredcore',
-		'salonbus1024',
-	];
-
-	const clean = () => {
-
-		tweetPage = /\/status\/\d+$/.test(window.location.pathname);
-
-		document.querySelectorAll('span').forEach(a => {
-			if (a?.innerText?.startsWith('Promoted')) {
-				hideArticle(a);
-			}
-		});
-
-		document.querySelectorAll('video, iframe').forEach((a) => {
-			hide(a?.closest('div[data-testid="tweetPhoto"]'));
-			hide(a);
-		});
-
-		document.querySelectorAll('div[data-testid=User-Name] img[title^="Flag of"], div[data-testid=User-Name] img[title="🇺🇦"]').forEach(hideArticle);
-
-		if (!document.querySelector('[data-testid="UserName"] svg[aria-label="Verified account"]')) {
-			document.querySelectorAll('svg[aria-label="Verified account"]').forEach(icon => {
-				const a = icon.closest('[data-testid="User-Name"]');
-				if (!a) {
-					return;
-				}
-				for (const s of whiteList) {
-					if (a.innerText.includes(s)) {
-						return;
-					}
-				}
-				hideArticle(a);
-			});
-		}
-
-		document.querySelectorAll('div[data-testid=User-Name] a span').forEach(a => {
-			if (a.innerText.includes('互fo')) {
-				hideArticle(a);
-			}
-		});
-
-		document.querySelectorAll(hideItem).forEach(forceHide);
-
-		document.querySelectorAll('[data-testid=tweetText]').forEach(a => {
-			a.style.lineHeight = 1.75;
-		});
-
-		document.querySelectorAll('[aria-label$="View Tweet analytics"]').forEach(a => {
-			hide(a.parentNode);
-		});
-	};
-
-	clean();
-	setInterval(clean, 500);
-})();
+GM_addStyle(`
+a[aria-label="社群"],
+a[aria-label="Premium"],
+div.mark-container {
+	display: none !important;
+}
+`);
