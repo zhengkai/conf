69d9f3cea21d327aa8934b3406aaadb83cf70f1e
Zheng Kai <zhengkai@gmail.com>
2026-04-23 19:58:34 +0800

    up

--- hypr/script/start-wezterm.sh
+++ hypr/script/start-wezterm.sh
@@ -7,3 +7,7 @@ flock -n 200 || exit
 set -x
 
 hyprctl dispatch exec "[workspace name:wezterm silent; fullscreen] wezterm start --always-new-process -- ${HOME}/conf/script/tmux-tilda.sh"
+
+sleep 3
+
+hyprctl dispatch exec "[workspace name:log silent; fullscreen] wezterm start --always-new-process -- ~/conf/bin/tmu"
--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -2,63 +2,63 @@
 
 # 根据 env FIGNORE 过滤 fzf 输出文件
 __zk_filter_fignore() {
-  emulate -L zsh
-  setopt extendedglob
+	emulate -L zsh
+	setopt extendedglob
 
-  local line pat suffix
-  local -a ignores pats
+	local line pat suffix
+	local -a ignores pats
 
-  ignores=(${(s.:.)FIGNORE})
+	ignores=(${(s.:.)FIGNORE})
 
-  (( ${#ignores} == 0 )) && { cat; return 0; }
+	(( ${#ignores} == 0 )) && { cat; return 0; }
 
-  pats=()
-  for suffix in "${ignores[@]}"; do
-    [[ -z $suffix ]] && continue
-    pats+=("${(b)suffix}")
-  done
+		pats=()
+		for suffix in "${ignores[@]}"; do
+			[[ -z $suffix ]] && continue
+			pats+=("${(b)suffix}")
+		done
 
-  (( ${#pats} == 0 )) && { cat; return 0; }
+		(( ${#pats} == 0 )) && { cat; return 0; }
 
-  pat="*(${(j:|:)pats})"
+			pat="*(${(j:|:)pats})"
 
-  while IFS= read -r line; do
-    [[ $line == ${~pat} ]] || print -r -- "$line"
-  done
+			while IFS= read -r line; do
+				[[ $line == ${~pat} ]] || print -r -- "$line"
+			done
 }
 
 # 从 git 根目录获取文件
 __zk_fsel() {
-  local root
-  root=$(git rev-parse --show-toplevel 2>/dev/null) || return 1
+	local root
+	root=$(git rev-parse --show-toplevel 2>/dev/null) || return 1
 
-  setopt localoptions pipefail
+	setopt localoptions pipefail
 
-  cdup=$(git rev-parse --show-cdup)
-  git ls-tree -r --name-only HEAD "${cdup:-.}" \
-    | __zk_filter_fignore \
-	| FZF_DEFAULT_OPTS="--height 40% --reverse" fzf "$@"
+	cdup=$(git rev-parse --show-cdup)
+	git ls-tree -r --name-only HEAD "${cdup:-.}" \
+		| __zk_filter_fignore \
+		| FZF_DEFAULT_OPTS="--height 40% --reverse" fzf "$@"
 }
 
 fzf-git-widget() {
 
-  local file ret
+	local file ret
 
-  if ! git rev-parse --show-toplevel >/dev/null 2>&1; then
-    zle fzf-file-widget
-    return $?
-  fi
+	if ! git rev-parse --show-toplevel >/dev/null 2>&1; then
+		zle fzf-file-widget
+		return $?
+	fi
 
-  file="$(__zk_fsel)"
-  ret=$?
+	file="$(__zk_fsel)"
+	ret=$?
 
-  if [[ -n $file ]]; then
-    BUFFER="vi ${(q)file}"
-    CURSOR=${#BUFFER}
-  fi
+	if [[ -n $file ]]; then
+		BUFFER="vi ${(q)file}"
+		CURSOR=${#BUFFER}
+	fi
 
-  zle reset-prompt
-  return $ret
+	zle reset-prompt
+	return $ret
 }
 
 zle -N fzf-git-widget
