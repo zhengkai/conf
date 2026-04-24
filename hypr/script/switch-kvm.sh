02038e3448156ecb0fe4283b246ce9e636cd4c03
Zheng Kai <zhengkai@gmail.com>
2026-04-24 11:35:46 +0800

    up

--- hypr/script/switch-kvm.sh
+++ hypr/script/switch-kvm.sh
@@ -4,7 +4,20 @@ VM_NAME="win11"
 
 hyprctl dispatch workspace "name:${VM_NAME}"
 
-set -x
+LOCK_FILE="${HOME}/dev/hypr/$(basename "$0")-${VM_NAME}.lock"
+exec 200>"$LOCK_FILE"
+flock -n 200 || exit
+
 if ! virsh list --state-running --name | grep -q "^${VM_NAME}$"; then
-	sudo virsh start "${VM_NAME}" &>/dev/null || :
+	sudo virsh start "${VM_NAME}" &
 fi
+
+if [ "$(hyprctl activeworkspace -j | jq '.windows')" -eq 0 ]; then
+	looking-glass-client -f /dev/shm/looking-glass
+fi
+
+sleep 3
+
+# if ! virsh list --state-running --name | grep -q "^${VM_NAME}$"; then
+# 	sudo virsh start "${VM_NAME}" &>/dev/null || :
+# fi
